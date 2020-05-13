50bfb17451b290604c3f891cb89852f2b724ac90
neritoo <ezegavilan95@gmail.com>
2020-05-13 20:34:08 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PathFinderComponent } from './components/path-finder/path-finder.component';
+import { NodeComponent } from './components/node/node.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PathFinderComponent,
+    NodeComponent
   ],
   imports: [
     BrowserModule
